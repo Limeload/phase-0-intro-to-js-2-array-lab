5ad0f1f7a631ad09c41bcca9b2973a90c8e64349
Shraddha Rao <shraddharaom@gmail.com>
2022-11-21 21:58:44 -0600

    commit tree 325b484235599678f385b4e93f1f3da10eaba244

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+  const cats = ["Milo", "Otis", "Garfield"];
+
+  function destructivelyAppendCat() {
+      return cats.push("Ralph");
+  }
+
+ function destructivelyPrependCat() {
+     return cats.unshift("Bob")
+ }
+
+ function destructivelyRemoveLastCat() {
+     return cats.pop();
+ }
+
+ function destructivelyRemoveFirstCat() {
+     return cats.shift();
+ }
+
+ const catsArray1 = ["Milo", "Otis", "Garfield", "Broom"];
+ function appendCat() {
+     return catsArray1.slice("Broom");
+ }
+
+ const catsArray2 = ["Arnold", "Milo", "Otis", "Garfield"];
+ function prependCat(){
+     return catsArray2.slice("Arnold");
+ }
+
+ function removeLastCat() {
+    return cats.slice(0, cats.length -1);
+}
+
+ function removeFirstCat() {
+     return cats.slice(1);
+ }
+
